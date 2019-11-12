6027cc96c624baebc4a69c22a885f1a211276ea9
Diana <ddianka.1917@gmail.com>
2019-11-12 17:26:30 +0300

    12/11

--- task12_11/index.js
+++ task12_11/index.js
@@ -4,15 +4,18 @@ document.getElementById('btn').addEventListener('click', () => { document.getEle
 // TASK 2
 
 const btn = document.getElementById('btn');
-btn.addEventListener('click', () => {btn.style="display: none"});
+btn.addEventListener('click', () => {
+    btn.style="display: none";
+});
 
 // TASK 3
-btn.addEventListener("click", () => alert ("1"));   // запустится
-btn.removeEventListener("click", () => alert("1") ); // не запустится
-btn.onclick = () => alert(2); // запустится
+// btn.addEventListener("click", () => alert ("1"));   // запустится
+// btn.removeEventListener("click", () => alert("1") ); // не запустится
+// btn.onclick = () => alert(2); // запустится
 
 // TASK 5
-const paragraph = document.querySelector('p');
-const menu = document.querySelector('ul');
-console.log('ih')
-paragraph.addEventListener('click', () => menu.classList.toggle('active'));
+const menuBtn = document.querySelector('#menu-btn');
+const menu = document.querySelector('.menu-list');
+console.log('menu', menu)
+console.log('menu', menuBtn)
+menuBtn.addEventListener('click', () => menu.classList.toggle('active'));
