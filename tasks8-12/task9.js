9c632d356e3684ac7e562c3732019b85885d32d0
Diana <ddianka.1917@gmail.com>
2019-11-19 01:50:47 +0300

    9 task

---
+++ tasks8-12/task9.js
@@ -0,0 +1,8 @@
+const age = document.querySelector('th[data-type="number"]');
+const name = document.querySelector('th[data-type="string"]');
+console.log(age)
+console.log(name)
+
+age.addEventListener('click', () => {
+    const tableRows = document.querySelectorAll('tr');
+})
