8cb1de9b90a3d0aef349f658244fc3a3f63aaf0b
Diana <ddianka.1917@gmail.com>
2019-11-19 03:12:50 +0300

    task 9

--- tasks8-12/task9.js
+++ tasks8-12/task9.js
@@ -1,8 +1,29 @@
-const age = document.querySelector('th[data-type="number"]');
-const name = document.querySelector('th[data-type="string"]');
-console.log(age)
-console.log(name)
+const tBody = document.querySelector('tbody');
+const tHeads = document.querySelectorAll('th');
 
-age.addEventListener('click', () => {
-    const tableRows = document.querySelectorAll('tr');
-})
+for(let i = 0; i < tHeads.length; i++){
+    tHeads[i].addEventListener('click', () => {
+        const tableRows = Array.from(
+            document.querySelectorAll('tbody tr')
+        );
+
+        const sortedRows = tableRows.sort((a,b) => {
+            const paramA = a.children[i].innerText;
+            const paramB= b.children[i].innerText;
+            if ( paramA === paramB) {
+                return 0
+            } else if (paramA > paramB) {
+                return 1
+            }
+            return -1
+        });
+
+        tBody.innerHTML = "";
+
+        for(let j = 0; j < sortedRows.length; j++) {
+            tBody.appendChild(sortedRows[j]);
+        }
+    });
+}
+
+
